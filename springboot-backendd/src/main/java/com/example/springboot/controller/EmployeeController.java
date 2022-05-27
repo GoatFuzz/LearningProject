494f9af78df22fe8e214471101ace3c2aa1405dd
GoatFuzz <tjm6843@psu.edu>
2022-05-26 21:38:12 -0500

    What is Happening

---
+++ springboot-backendd/src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.model.Employee;
+import com.example.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+}
